23478032782c451dd21b4acf07e6104bd202bffd
RomainBarraud <romain.barraud@gmail.com>
2019-12-16 01:27:02 +0800

    create main for pose detection

--- main.py
+++ main.py
@@ -4,11 +4,26 @@ import numpy as np
 
 from cv2 import cv2
 
-protoFile = "pose/coco/pose_deploy_linevec.prototxt"
-weightsFile = "pose/coco/pose_iter_440000.caffemodel"
+protoFile = "pose-detection/pose/coco/pose_deploy_linevec.prototxt"
+weightsFile = "pose-detection/pose/coco/pose_iter_440000.caffemodel"
+
+BODY_DICT = {'nose': 0,
+    'neck': 1, 'shoulder_right': 2, 'elbow_right': 3, 'wrist_right': 4, 'shoulder_left': 5,
+    'elbow_left': 6, 'wrist_left': 7, 'hip_right': 8, 'knee_right': 9, 'ankle_right':10,
+    'hip_left': 11, 'knee_left': 12, 'ankle_left': 13, 'eyebrow_right': 14, 'eyebrow_left': 15,
+    'ear_right': 16, 'ear_left': 17
+}
+
+BODY_DICTIONNARY_2 = {'0': 'nose',
+    '1': 'neck', '2': 'shoulder_right', '3': 'elbow_right', '4': 'wrist_right', '5': 'shoulder_left',
+    '6': 'elbow_left', '7': 'wrist_left', '8': 'hip_right', '9': 'knee_right', '10': 'ankle_right',
+    '11': 'hip_left', '12': 'knee_left', '13': 'ankle_left', '14': 'eyebrow_right', '15': 'eyebrow_left',
+    '16': 'ear_right', '17': 'ear_left'
+}
+
 nPoints = 18
 POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[1,8],[8,9],[9,10],[1,11],[11,12],[12,13],[0,14],[0,15],[14,16],[15,17]]
-
+POSE_HEAD = [ [BODY_DICT['ear_right'], BODY_DICT['ear_left']], [BODY_DICT['ear_left'], BODY_DICT['neck']], [BODY_DICT['neck'], BODY_DICT['ear_right']] ]
 
 app = Flask(__name__)
 
@@ -21,7 +36,7 @@ def pose_main():
     #myPose = data["myPose"]
 
     print("will read image")
-    frame = cv2.imread("single.jpeg")
+    frame = cv2.imread("./client_images/images_input/black-male.jpg")
     frameCopy = np.copy(frame)
     frameWidth = frame.shape[1]
     frameHeight = frame.shape[0]
@@ -60,8 +75,8 @@ def pose_main():
         y = (frameHeight * point[1]) / H
 
         if prob > threshold : 
-            cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-            cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+            #cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+            #cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
 
             # Add the point to the list if the probability is greater than the threshold
             points.append((int(x), int(y)))
@@ -69,24 +84,31 @@ def pose_main():
             points.append(None)
 
     # Draw Skeleton
-    for pair in POSE_PAIRS:
+    #for pair in POSE_PAIRS:
+        #partA = pair[0]
+        #partB = pair[1]
+
+        #if points[partA] and points[partB]:
+            #cv2.line(frame, points[partA], points[partB], (0, 255, 255), 2)
+            #cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+
+    # Draw Head Frame
+    for pair in POSE_HEAD:
         partA = pair[0]
         partB = pair[1]
 
         if points[partA] and points[partB]:
             cv2.line(frame, points[partA], points[partB], (0, 255, 255), 2)
-            cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
 
+    cv2.imshow('Output-Keypoints', frameCopy)
+    cv2.imshow('Output-Skeleton', frame)
 
-    #cv2.imshow('Output-Keypoints', frameCopy)
-    #cv2.imshow('Output-Skeleton', frame)
-
-    cv2.imwrite('Output-Keypoints.jpg', frameCopy)
-    cv2.imwrite('Output-Skeleton.jpg', frame)
+    #cv2.imwrite('./client_images/images_output/Output-Keypoints.jpg', frameCopy)
+    #cv2.imwrite('./client_images/images_output/Output-Skeleton.jpg', frame)
 
     print("Total time taken : {:.3f}".format(time.time() - t))
 
-    #cv2.waitKey(0)
+    cv2.waitKey(0)
 
     return jsonify({"status": "success"})
 
